2448438e541b6e673231ac9cad048e30d7dd2bc3
Radek <rogbin17@gmail.com>
2020-02-11 15:37:16 +0100

    Stworzono modele - player, club, operator, referee, tournament.

---
+++ Manager_Turnieju/Models/Referee.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Manager_Turnieju.Models
+{
+    public class Referee
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+    }
+}
